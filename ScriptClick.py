4c613c82914b400142a760effe9a1425fe09963a
ChaterHelperApiKoroch <153524623+ChaterHelperApiKoroch@users.noreply.github.com>
2024-03-19 15:39:54 +0300

    Update ScriptClick.py

--- ScriptClick.py
+++ ScriptClick.py
@@ -10,7 +10,7 @@ async def send_click(url, headers, data, session):
 
 async def click(col, pov):
     url = "https://arbuz.betty.games/api/click/apply"
-    headers = {"User-Agent": UserAgent().random, "X-Telegram-Init-Data": "query_id=AAG8y3hYAAAAALzLeFiujN6f&user=%7B%22id%22%3A1484311484%2C%22first_name%22%3A%22Jason%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22JasonVurhyz%22%2C%22language_code%22%3A%22ru%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1706804808&hash=96ebb8f837ad07e8d5712db65e5afc78c1c95fe7b7477d4c868255a5ff70fe40"}
+    headers = {"User-Agent": UserAgent().random, "X-Telegram-Init-Data": "query_id=AAEF9sYDAwAAAAX2xgPhGHwQ&user=%7B%22id%22%3A6505821701%2C%22first_name%22%3A%22f0x3r0k%22%2C%22last_name%22%3A%22%22%2C%22username%22%3A%22f0x9r0k%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1710851623&hash=34ba4dbc4f61830bc892d7d3c082c8ed0c18ab56c8c01bc779b6ca26a845cde8"}
     data = {"count": 10, "hash": "ca1a97a3cdec77b7e1e0de9f420c296bd057f3a82a1e76a1a1dde2f84d73d23f"}
 
     async with aiohttp.ClientSession() as session:
@@ -22,7 +22,7 @@ async def click(col, pov):
 
 
 while True:
-	asyncio.run(click(10, 900))
+	asyncio.run(click(10, 200))
 	sleep(15)
 	os.system("cls||clear")
 	print("Lets GO")
